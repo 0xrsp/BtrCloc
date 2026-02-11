80b51caaeb2c55d4aef801213a2dc0aa13c53dc7
mmmm <95966883+0xrsp@users.noreply.github.com>
2026-02-11 21:23:11 +1030

    add a bunch of exts

--- main.cpp
+++ main.cpp
@@ -1,14 +1,52 @@
 #include "winapi.hpp"
 #include "cliargs.cpp"
 
-// TODO: Add more file types
 enum Enum_File_Type : s32 {
   FILETYPE_UNKNOWN = 0,
+  //data formats
+  FILETYPE_XML,
+  FILETYPE_JSON,
+  FILETYPE_TOML,
+  FILETYPE_YAML,
+  FILETYPE_PROPERTIES,
+  FILETYPE_INI,
+
+  //c
   FILETYPE_H_HEADER,
   FILETYPE_HPP_HEADER,
   FILETYPE_C_SRC,
   FILETYPE_CXX_SRC,
+  FILETYPE_ASM,
+
+  //random langs
+  FILETYPE_PYTHON,
+  FILETYPE_PHP,
+  FILETYPE_CSHARP,
+  FILETYPE_GOLANG,
+  FILETYPE_RUST,
+  FILETYPE_RUBY,
+  FILETYPE_ZIG,
+  FILETYPE_SWIFT,
   FILETYPE_JAVA,
+  FILETYPE_GRADLE,
+  FILETYPE_KOTLIN,
+  FILETYPE_HASKELL,
+  FILETYPE_JULIA,
+
+  //web stuff
+  FILETYPE_JAVASCRIPT,
+  FILETYPE_TYPESCRIPT,
+  FILETYPE_HTML,
+  FILETYPE_CSS,
+  FILETYPE_VUE,
+  FILETYPE_SVELTE,
+
+  //other
+  FILETYPE_BAT,
+  FILETYPE_POWERSHELL,
+  FILETYPE_MARKDOWN,
+  FILETYPE_SH,
+
   FILETYPES_COUNT,
 };
 
@@ -22,15 +60,60 @@ enum Enum_File_Type : s32 {
     return (n)
 
 
-// TODO: Could use some kind of precomputed table
 static Enum_File_Type MapExtToFileType(const s8* ext) {
   if (strcmp(ext, "c") == 0) {
     return FILETYPE_C_SRC;
   }
-  FILETYPE_EXT_MACRO(ext, "cpp", FILETYPE_CXX_SRC)
+// TODO: this should be some kind of hashtable
+  FILETYPE_EXT_MACRO(ext, "xml", FILETYPE_XML)
+    FILETYPE_EXT_MACRO(ext, "json", FILETYPE_JSON)
+    FILETYPE_EXT_MACRO(ext, "toml", FILETYPE_TOML)
+    FILETYPE_EXT_MACRO(ext, "yaml", FILETYPE_YAML)
+    FILETYPE_EXT_MACRO(ext, "yml", FILETYPE_YAML)
+    FILETYPE_EXT_MACRO(ext, "properties", FILETYPE_PROPERTIES)
+    FILETYPE_EXT_MACRO(ext, "ini", FILETYPE_INI)
+
+    FILETYPE_EXT_MACRO(ext, "cpp", FILETYPE_CXX_SRC)
+    FILETYPE_EXT_MACRO(ext, "cxx", FILETYPE_CXX_SRC)
+    FILETYPE_EXT_MACRO(ext, "c++", FILETYPE_CXX_SRC)
+    FILETYPE_EXT_MACRO(ext, "cc", FILETYPE_CXX_SRC)
     FILETYPE_EXT_MACRO(ext, "hpp", FILETYPE_HPP_HEADER)
     FILETYPE_EXT_MACRO(ext, "h", FILETYPE_H_HEADER)
+    FILETYPE_EXT_MACRO(ext, "s", FILETYPE_ASM)
+    FILETYPE_EXT_MACRO(ext, "asm", FILETYPE_ASM)
+
+    FILETYPE_EXT_MACRO(ext, "py", FILETYPE_PYTHON)
+    FILETYPE_EXT_MACRO(ext, "pyi", FILETYPE_PYTHON)
+    FILETYPE_EXT_MACRO(ext, "cs", FILETYPE_CSHARP)
+    FILETYPE_EXT_MACRO(ext, "go", FILETYPE_GOLANG)
+    FILETYPE_EXT_MACRO(ext, "rs", FILETYPE_RUST)
+    FILETYPE_EXT_MACRO(ext, "rb", FILETYPE_RUBY)
+    FILETYPE_EXT_MACRO(ext, "zig", FILETYPE_ZIG)
+    FILETYPE_EXT_MACRO(ext, "swift", FILETYPE_SWIFT)
+    FILETYPE_EXT_MACRO(ext, "gradle", FILETYPE_GRADLE)
     FILETYPE_EXT_MACRO(ext, "java", FILETYPE_JAVA)
+    FILETYPE_EXT_MACRO(ext, "kt", FILETYPE_KOTLIN)
+    FILETYPE_EXT_MACRO(ext, "kts", FILETYPE_KOTLIN)
+    FILETYPE_EXT_MACRO(ext, "hs", FILETYPE_HASKELL)
+    FILETYPE_EXT_MACRO(ext, "jl", FILETYPE_JULIA)
+
+    FILETYPE_EXT_MACRO(ext, "js", FILETYPE_JAVASCRIPT)
+    FILETYPE_EXT_MACRO(ext, "ts", FILETYPE_TYPESCRIPT)
+    FILETYPE_EXT_MACRO(ext, "jsx", FILETYPE_JAVASCRIPT)
+    FILETYPE_EXT_MACRO(ext, "tsx", FILETYPE_TYPESCRIPT)
+    FILETYPE_EXT_MACRO(ext, "html", FILETYPE_HTML)
+    FILETYPE_EXT_MACRO(ext, "hbs", FILETYPE_HTML)
+    FILETYPE_EXT_MACRO(ext, "css", FILETYPE_CSS)
+    FILETYPE_EXT_MACRO(ext, "scss", FILETYPE_CSS)
+    FILETYPE_EXT_MACRO(ext, "sass", FILETYPE_CSS)
+    FILETYPE_EXT_MACRO(ext, "less", FILETYPE_CSS)
+    FILETYPE_EXT_MACRO(ext, "svelte", FILETYPE_SVELTE)
+    FILETYPE_EXT_MACRO(ext, "vue", FILETYPE_VUE)
+
+    FILETYPE_EXT_MACRO(ext, "bat", FILETYPE_BAT)
+    FILETYPE_EXT_MACRO(ext, "ps1", FILETYPE_POWERSHELL)
+    FILETYPE_EXT_MACRO(ext, "md", FILETYPE_MARKDOWN)
+    FILETYPE_EXT_MACRO(ext, "sh", FILETYPE_SH)
   else {
     return FILETYPE_UNKNOWN;
   }
@@ -38,11 +121,42 @@ static Enum_File_Type MapExtToFileType(const s8* ext) {
 
 static const s8* FileTypeToStr(Enum_File_Type file_type) {
   switch (file_type) {
+    FILETYPE_NAME_MACRO(FILETYPE_XML, "XML");
+    FILETYPE_NAME_MACRO(FILETYPE_JSON, "JSON");
+    FILETYPE_NAME_MACRO(FILETYPE_TOML, "TOML");
+    FILETYPE_NAME_MACRO(FILETYPE_YAML, "YAML");
+    FILETYPE_NAME_MACRO(FILETYPE_PROPERTIES, "Properties");
+    FILETYPE_NAME_MACRO(FILETYPE_INI, "INI");
+
     FILETYPE_NAME_MACRO(FILETYPE_H_HEADER, "C/C++ Header");
     FILETYPE_NAME_MACRO(FILETYPE_HPP_HEADER, "C++ Header");
     FILETYPE_NAME_MACRO(FILETYPE_C_SRC, "C");
     FILETYPE_NAME_MACRO(FILETYPE_CXX_SRC, "C++");
+    FILETYPE_NAME_MACRO(FILETYPE_ASM, "ASM");
+
+    FILETYPE_NAME_MACRO(FILETYPE_PYTHON, "Python");
+    FILETYPE_NAME_MACRO(FILETYPE_CSHARP, "C#");
+    FILETYPE_NAME_MACRO(FILETYPE_GOLANG, "Go");
+    FILETYPE_NAME_MACRO(FILETYPE_RUST, "Rust");
+    FILETYPE_NAME_MACRO(FILETYPE_RUBY, "Ruby");
+    FILETYPE_NAME_MACRO(FILETYPE_ZIG, "Zig");
+    FILETYPE_NAME_MACRO(FILETYPE_SWIFT, "Swift");
+    FILETYPE_NAME_MACRO(FILETYPE_GRADLE, "Gradle");
     FILETYPE_NAME_MACRO(FILETYPE_JAVA, "Java");
+    FILETYPE_NAME_MACRO(FILETYPE_KOTLIN, "Kotlin");
+    FILETYPE_NAME_MACRO(FILETYPE_HASKELL, "Haskell");
+    FILETYPE_NAME_MACRO(FILETYPE_JULIA, "Julia");
+
+    FILETYPE_NAME_MACRO(FILETYPE_JAVASCRIPT, "JavaScript");
+    FILETYPE_NAME_MACRO(FILETYPE_TYPESCRIPT, "TypeScript");
+    FILETYPE_NAME_MACRO(FILETYPE_HTML, "HTML");
+    FILETYPE_NAME_MACRO(FILETYPE_CSS, "CSS");
+    FILETYPE_NAME_MACRO(FILETYPE_SVELTE, "Svelte");
+    FILETYPE_NAME_MACRO(FILETYPE_VUE, "Vue");
+    FILETYPE_NAME_MACRO(FILETYPE_BAT, "Batch");
+    FILETYPE_NAME_MACRO(FILETYPE_POWERSHELL, "PowerShell");
+    FILETYPE_NAME_MACRO(FILETYPE_MARKDOWN, "Markdown");
+    FILETYPE_NAME_MACRO(FILETYPE_SH, "Shell");
   default:
     return "Unknown";
   }
@@ -175,10 +289,12 @@ s32 main(s32 argc, s8* argv[]) {
   printf("Type\t\tCode\tBlank\tComment\n");
   for (s32 typei = FILETYPE_UNKNOWN + 1; typei < FILETYPES_COUNT; ++typei) {
     File_Result result = results[typei];
-    Enum_File_Type type = (Enum_File_Type)typei;
-    const s8* str = FileTypeToStr(type);
-    s32 tabs = 16 - strlen(str);
-    printf("%s%*s%d\t%d\t%d\n", str, tabs, "", result.code, result.blank, result.comment);
+    if (result.blank + result.code + result.comment > 0) {
+      Enum_File_Type type = (Enum_File_Type)typei;
+      const s8* str = FileTypeToStr(type);
+      s32 tabs = 16 - strlen(str);
+      printf("%s%*s%d\t%d\t%d\n", str, tabs, "", result.code, result.blank, result.comment);
+    }
   }
 
   return 0;
